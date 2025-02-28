d3bbef09c61a0f304682dffde02876e763193e41
mabohao <fengchuidujiangsheng@xinqijitech.com>
2025-02-28 15:45:11 +0800

    first commit

---
+++ src/main/java/org/example/entity/ChargerWeekTime.java
@@ -0,0 +1,32 @@
+package org.example.entity;
+
+public class ChargerWeekTime {
+    private String week;
+    private String hour;
+    private String duration;
+
+    public String getWeek() {
+        return week;
+    }
+
+    public void setWeek(String week) {
+        this.week = week;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+}
